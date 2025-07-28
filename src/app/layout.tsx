e5cae777ae1a57ad34a3e9bb7c175ffc1c016468
Charlie Kasov <CharlieKasov@gmail.com>
2025-07-28 14:03:29 +0000

    OK you've hit a snag twice trying to implement this change. Here's the e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'MathMog',
   description: 'Flex your mental math muscle',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#7395AE" />
       </head>
       <body className="font-body antialiased">
         {children}
