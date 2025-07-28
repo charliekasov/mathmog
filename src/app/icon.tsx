f63562330332713ccf8c06c95a0df97e7cba771a
Charlie Kasov <CharlieKasov@gmail.com>
2025-07-28 13:53:29 +0000

    So I'm seeing error logs that look like this for the site operation.  Th

---
+++ src/app/icon.tsx
@@ -0,0 +1,6 @@
+// This file is intentionally blank.
+// It prevents 404 errors from browsers requesting a favicon.
+// See https://nextjs.org/docs/app/api-reference/file-conventions/metadata/icon
+export function Icon() {
+  return null;
+}
