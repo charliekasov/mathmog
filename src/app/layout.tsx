da4504ccf35507ec469bc3a6b788a212d473c839
Charlie Kasov <CharlieKasov@gmail.com>
2025-08-10 18:17:55 +0000

    Could we try the Lato font?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#7395AE" />
       </head>
       <body className="font-body antialiased">
